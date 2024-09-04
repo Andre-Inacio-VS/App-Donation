874b55e4b31986ddcdc4c8a425715f8e1c29173c
andre.silva <ANDRE.SILVA@PROTENDIT.COM.BR>
2024-09-04 17:15:17 -0300

    installing capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourcompany.appdonations',
+  appName: 'App Donations',
+  webDir: 'www'
+};
+
+export default config;
